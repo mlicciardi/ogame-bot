7b3ae7c1b1bbb305c4342c3497a5198b2394c09c
Mattia Licciardi <mattia.licciardi@gmail.com>
2019-12-05 00:48:33 +0000

    main()

---
+++ src/index.ts
@@ -0,0 +1,46 @@
+import { launch, Page } from 'puppeteer';
+
+const loginUrl = 'https://lobby.ogame.gameforge.com/en_GB/hub';
+
+async function main() {
+  const browser = await launch({ headless: true });
+
+  const page = await browser.newPage();
+
+  await page.goto(loginUrl, { waitUntil: 'load' });
+
+  await page.screenshot({ path: `${Date.now()}.png` });
+
+  await screenshot(page);
+
+  await performClickOnElement(page, 'span', 'Log in');
+
+  await screenshot(page);
+
+  await page.close();
+  browser.close();
+}
+
+const screenshot = async (page: Page) => {
+  await page.screenshot({ path: `tmp/${Date.now()}.png` });
+};
+
+const performClickOnElement = async (page: Page, tag: string, text: string) => {
+  const escapedText = escapeXpathString(text);
+  const linkHandlers = await page.$x(
+    `//${tag}[contains(text(), ${escapedText})]`,
+  );
+
+  if (linkHandlers.length > 0) {
+    await linkHandlers[0].click();
+  } else {
+    throw new Error(`Link not found: ${text}`);
+  }
+};
+
+const escapeXpathString = (searchString: string) => {
+  const splitedQuotes = searchString.replace(/'/g, `', "'", '`);
+  return `concat('${splitedQuotes}', '')`;
+};
+
+main();
